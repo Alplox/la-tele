26b5b8a296ccc602f60f1995984f275b51613c08
Alplox <42575576+Alplox@users.noreply.github.com>
2024-05-14 16:45:45 -0400

    cambio momentaneo
    Para evitar problemas con nuevo formato canales

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,7 +174,7 @@ BOTON_ALTERNAR_VISIBILIDAD_OVERLAY.addEventListener('click', () => {
 });
 
 // MARK: Fetch primario
-fetch('https://raw.githubusercontent.com/Alplox/json-teles/main/canales.json')
+fetch('https://raw.githubusercontent.com/Alplox/teles/92d8b002d10933c6d54631acffaf6d53034f04e8/assets/data/canales.json')
   .then(response => response.json())
   .then(data => {
     const FRAGMENT_CONTENEDOR_BOTONES_LISTA_PRINCIPAL = document.createDocumentFragment();
